4623e91ed3647e8e19e24fda1118f7095dbdbe1d
Jiawei Zhao <106795447+Jiawei-Zhao-728@users.noreply.github.com>
2026-07-06 01:41:12 -0400

    Initial Commit

---
+++ SplitLens/SplitLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitLensApp.swift
+//  SplitLens
+//
+//  Created by Jiawei Zhao on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SplitLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
